e3811487ca77a6872de4e5076c81353fdef1e082
Marcos29112020 <mpoliveiras122020@gmail.com>
2025-02-27 19:19:07 -0300

    Melhoria continua

--- sw.js
+++ sw.js
@@ -1,26 +1,31 @@
 const CACHE_NAME = 'm-economy-cache-v1';
 const urlsToCache = [
-    '/price-compare-app/',
-    '/price-compare-app/index.html',
-    '/price-compare-app/style.css',
-    '/price-compare-app/script.js',
-    '/price-compare-app/manifest.json',
-    '/price-compare-app/icon.png',
-    '/price-compare-app/icon-large.png'
+    '/',
+    '/index.html',
+    '/style.css',
+    '/script.js',
+    '/manifest.json',
+    '/icon.png',
+    '/icon-large.png'
 ];
 
 self.addEventListener('install', event => {
     event.waitUntil(
         caches.open(CACHE_NAME)
             .then(cache => cache.addAll(urlsToCache))
+            .then(() => self.skipWaiting())
             .catch(err => console.log('Erro ao cachear:', err))
     );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(self.clients.claim());
 });
 
 self.addEventListener('fetch', event => {
     event.respondWith(
         caches.match(event.request)
             .then(response => response || fetch(event.request))
-            .catch(() => caches.match('/price-compare-app/index.html'))
+            .catch(() => caches.match('/index.html'))
     );
 });
